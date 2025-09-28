1b00ff5b8cbe43565c944e6aa2333c44278caa73
howardrose <philip.anthon@gmail.com>
2025-09-28 19:01:59 +0800

    asmfunc2.asm (no boundary check implemented yet)

--- simd_integration/asmfunc2.asm
+++ simd_integration/asmfunc2.asm
@@ -13,6 +13,35 @@ section .text
 global matvec_xmm_avx2_asm
 
 matvec_xmm_avx2_asm:
-    ret
-     
+    xor rsi, rsi ;outer loop counter i
+
+loop_outer:
+    xorps xmm0, xmm0 ;result per row    
+    mov r10, rsi
+    imul r10, rcx
+    lea r11, [rdx + r10 * 4] ;row (&A[i])
+    xor rdi, rdi ;inner loop counter j
+
+loop_inner:
+    vmovdqu xmm1, [r11 + rdi * 4]
+    vmovdqu xmm2, [r8 + rdi * 4]
+    vmulps xmm1, xmm1, xmm2
+    vaddps xmm0, xmm0, xmm1
+    
+    add rdi, 4
+    cmp rdi, rcx
+    jl loop_inner
+
+store:
+    vhaddps xmm0, xmm0
+    vhaddps xmm0, xmm0
+    vmovss [r9 + rsi * 4], xmm0
+    
+    inc rsi
+    cmp rsi, rcx
+    jl loop_outer
+
+done:
+    ret     
+   
    
